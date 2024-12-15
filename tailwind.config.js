9759118ebc48a5f94bb3e277c48857e652ec0574
Juliano Sill <sill.juliano@gmail.com>
2024-12-15 17:25:10 -0300

    feat(html): style scroll bar and update bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,5 +53,5 @@ export default {
     },
   },
   // eslint-disable-next-line @typescript-eslint/no-require-imports
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')],
 }
