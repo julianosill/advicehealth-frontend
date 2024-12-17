65b2e3b68d3090ac66ccd6ec858514e36e13c824
Juliano Sill <sill.juliano@gmail.com>
2024-12-17 11:42:04 -0300

    chore: minor component adjusments

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,13 +1,30 @@
 import { cn } from '@/lib/utils'
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+  quantity?: number
+}
 
 export function Skeleton({
   className,
+  quantity = 1,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: Readonly<SkeletonProps>) {
+  const skeletonNumber = Array.from(
+    { length: quantity },
+    (_, index) => index + 1,
+  )
+
   return (
-    <div
-      className={cn('w-full animate-pulse rounded-md bg-muted', className)}
-      {...props}
-    />
+    <>
+      {skeletonNumber.map(index => (
+        <div
+          key={index}
+          className={cn(
+            'w-full animate-pulse rounded-md bg-muted/75',
+            className,
+          )}
+          {...props}
+        />
+      ))}
+    </>
   )
 }
